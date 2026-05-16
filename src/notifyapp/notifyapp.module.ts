11bacad57fada7c7ad6058bff992da0bfe79d3d8
tawanza11 <petchanan7051@email.com>
2026-05-16 14:28:18 +0700

    feat: add FeatureFlagsModule to NotifyRtModule imports

--- src/notifyapp/notifyapp.module.ts
+++ src/notifyapp/notifyapp.module.ts
@@ -4,10 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ShoppingOrderEntity } from 'src/shopping-order/shopping-order.entity';
 import { NotificationTokenEntity } from './notification-token.entity';
 import { NotifyRtController } from './notifyapp.controller';
+import { FeatureFlagsModule } from '../feature-flags/feature-flags.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([ShoppingOrderEntity, NotificationTokenEntity]),
+    FeatureFlagsModule,
   ],
   controllers: [NotifyRtController],
   providers: [NotifyRtService],
