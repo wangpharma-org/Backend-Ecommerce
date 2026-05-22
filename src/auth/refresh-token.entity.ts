db56e53facdabec5737fc365f6869b09eca97d6c
SASIT SUANSADANG <sasit.x10@gmail.com>
2026-05-22 16:33:24 +0700

    perf: optimize refresh token endpoint
    - add @Index on refresh_token column to eliminate full table scan
    - parallelize userRepo.findOne + refreshTokenRepo.delete with Promise.all
    - parallelize jwtService.signAsync x2 with Promise.all
    - select only required user fields (14/53 columns) to reduce data transfer
    - replace console.log with NestJS Logger (R-001)

--- src/auth/refresh-token.entity.ts
+++ src/auth/refresh-token.entity.ts
@@ -1,10 +1,11 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, Index } from 'typeorm';
 
 @Entity({ name: 'reflesh-token' })
 export class RefreshTokenEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Index()
   @Column({ type: 'varchar', length: 512 })
   refresh_token: string;
 
