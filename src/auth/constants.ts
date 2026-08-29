53d394ddfce3fd1de5a0f4c40ede10fe09f39ab6
MossOcelot <108613206+MossOcelot@users.noreply.github.com>
2026-08-29 18:38:28 +0700

    Update constants.ts

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,3 +1,3 @@
 export const jwtConstants = {
-  secret: process.env.ACCESS_TOKEN_SECRET || 'R4KE0ica1A2eJVFVCHZm5am8OUqox4ZwN1hByjUF2fS3JQ2KcW4pRXKvWSVLlxdbp0f4NT2UDtq+PBF+Od6IWQ==',
+  secret: process.env.ACCESS_TOKEN_SECRET || 'fallback-secret',
 };
